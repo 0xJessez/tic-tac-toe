8a3b4e7b61a823daee3987cbd09874ecec294208
0xJessez <jesse.zhang919@gmail.com>
2022-11-23 14:47:16 +1100

    draft game logic added

--- app.js
+++ app.js
@@ -15,3 +15,64 @@
 // 3. 
     // There are 12 possible combinations of winning and this can be tested for after each click using an if function
     // Each box will need to be tracked to run above if function, can be done using a unique ID for each box
+
+
+// Declaring general grid elements / variables
+var grid = document.querySelector('.grid')
+var box = document.querySelector('.box')
+var knot = document.createElement('div')
+var innerKnot = document.createElement('div')
+knot.id = 'outer-circle'
+innerKnot.id = 'inner-circle'
+knot.appendChild(innerKnot)
+
+var cross1 = document.createElement('div')
+var cross2 = document.createElement('div')
+cross1.classList.add('cross1')
+cross2.classList.add('cross2')
+cross1.appendChild(cross2)
+
+var statusBar = document.querySelector('.statusBar')
+
+var count = 0
+
+// Declaring individual boxes
+var box1 = document.querySelector('#box1')
+var box2 = document.querySelector('#box2')
+var box3 = document.querySelector('#box3')
+var box4 = document.querySelector('#box4')
+var box5 = document.querySelector('#box5')
+var box6 = document.querySelector('#box6')
+var box7 = document.querySelector('#box7')
+var box8 = document.querySelector('#box8')
+var box9 = document.querySelector('#box9')
+
+// Array to track player moves
+var winCon = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+// Game logic
+grid.addEventListener('click', function (event) {
+    box = event.target
+    if(box.className === 'box' && count % 2 === 0) {
+        box.appendChild(knot.cloneNode(true))
+        // box.appendChild(cross1.cloneNode(true))
+        box.classList.add('o')
+        winCon.splice(box.id.slice(-1)-1, 1, 1)
+        count++
+        console.log(count)
+
+    } else if (box.className === 'box' && count % 2 !== 0) {
+        box.appendChild(cross1.cloneNode(true))
+        box.classList.add('x')
+        winCon.splice(box.id.slice(-1)-1, 1, 2)
+        count++
+        console.log(count)
+    }
+    // if (box1.className === 'box o' && box2.className === 'box o' && box3.className === 'box o' || box1.className === 'box o' && box2.className === 'box o' && box3.className === 'box o' || box1.className === 'box o' && box2.className === 'box o' && box3.className === 'box o' || box1.className === 'box o' && box2.className === 'box o' && box3.className === 'box o' ||) {
+    //     statusBar.textContent = "Player 1 WINS"
+    // } 
+})
+
+// wincon loops
+
+
